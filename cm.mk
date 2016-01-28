56a0aafe2ccf33fe86e1a0ac5eaac8f203c2c680
Ethan Chen <intervigil@gmail.com>
2016-01-28 15:44:41 -0800

    hiaeuhl: Use correct fingerprint
    Change-Id: I1d0fed24fb09fdce322de2c7a84543447f799020

--- cm.mk
+++ cm.mk
@@ -13,6 +13,6 @@ PRODUCT_DEVICE := hiaeuhl
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_hiaeuhl" \
-    PRODUCT_NAME="hiaeul_00617" \
-    BUILD_FINGERPRINT="htc/hiaeul_00617/htc_hiaeul:6.0/MRA58K/639737.6:user/release-keys" \
+    PRODUCT_NAME="hiaeuhl_00617" \
+    BUILD_FINGERPRINT="htc/hiaeuhl_00617/htc_hiaeul:6.0/MRA58K/639737.6:user/release-keys" \
     PRIVATE_BUILD_DESC="1.12.617.6 CL639737 release-keys"
