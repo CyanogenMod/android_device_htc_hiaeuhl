d8e7008d861a551dd3fe9d121e680b717dda7694
Ethan Chen <intervigil@gmail.com>
2016-11-26 23:47:49 -0800

    hiaeuhl: Remove enhanced NFC feature
    Change-Id: I33463fef13c440182fd0ab5850657cc52e2325d9

--- cm.mk
+++ cm.mk
@@ -3,9 +3,6 @@ $(call inherit-product, device/htc/hiaeuhl/full_hiaeuhl.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 PRODUCT_NAME := cm_hiaeuhl
 BOARD_VENDOR := htc
 TARGET_VENDOR := htc
